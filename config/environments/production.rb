a73e001eeb4f4711995fae3db924007492db05af
ATUL NIKHADE <ATSNIKHADE@GMAIL.com>
2018-07-24 22:24:52 +0530

    imags nt dispay

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.serve_static_files = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -61,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "atm_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "academy_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
